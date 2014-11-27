21c52b30ad2d7d265ae197b9d27978550c5f3d3c
jjy05 <jjy05@fi--dide276.dide.local>
2014-11-27 15:43:31 +0000

    CSimulator header file updated

--- CSimulator.h
+++ CSimulator.h
@@ -4,3 +4,13 @@
  *  Created on: 26 Nov 2014
  *      Author: Jie Yang
  */
+
+#include <fstream>
+
+#define BUFFER_SIZE 1024
+
+class CSimulator {
+public:
+	CSimulator();
+	virtual ~CSimulator();
+};
