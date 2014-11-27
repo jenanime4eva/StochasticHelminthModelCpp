7d2c38a49bbfb83b2bbd675c7168ebadf6ddf9ad
jjy05 <jjy05@fi--dide276.dide.local>
2014-11-27 15:39:55 +0000

    CHost header file created

---
+++ CHost.h
@@ -0,0 +1,16 @@
+/*
+ * CHost.h
+ *
+ *  Created on: 27 Nov 2014
+ *      Author: Jie Yang
+ */
+
+#include <fstream>
+
+#define BUFFER_SIZE 1024
+
+class CHost {
+public:
+	CHost();
+	virtual ~CHost();
+};
